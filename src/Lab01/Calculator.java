74d14b002d5d2e76184b05c4794a58e57ddd5e1a
for-Ely <kaneki21174@gmail.com>
2024-03-15 15:58:00 +0700

    Change name for Lab1 to Lab01

--- src/Lab01/Calculator.java
+++ src/Lab01/Calculator.java
@@ -12,7 +12,13 @@ public class Calculator {
         JOptionPane.showMessageDialog(null, "Sum of them is: "+(douNum1+douNum2), "Sum", JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Difference of them is: " + (douNum1-douNum2), "Difference", JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Product of them is: " + (douNum1*douNum2), "Product", JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Quotient of them is: " + (douNum1/douNum2), "Quotient", JOptionPane.INFORMATION_MESSAGE);
+        if (douNum2 != 0){
+            JOptionPane.showMessageDialog(null, "Quotient of them is: " + (douNum1/douNum2), "Quotient", JOptionPane.INFORMATION_MESSAGE);
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Devisor is not allow to be 0", "Quotient", JOptionPane.INFORMATION_MESSAGE);
+            
+        }
         System.exit(0);
     }
 }
