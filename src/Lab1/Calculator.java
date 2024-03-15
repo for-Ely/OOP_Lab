14e98752939f5c705e8cbf02eb0ee63aec5d266f
for-Ely <kaneki21174@gmail.com>
2024-03-15 15:43:29 +0700

    Add calculator

---
+++ src/Lab1/Calculator.java
@@ -0,0 +1,18 @@
+package Lab1;
+import javax.swing.JOptionPane;
+public class Calculator {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+        double douNum1, douNum2;
+        strNum1 = JOptionPane.showInputDialog(null, "Please type the first number", "Get first number", JOptionPane.INFORMATION_MESSAGE);
+        strNum2 = JOptionPane.showInputDialog(null, "Please type the second number", "Get second number", JOptionPane.INFORMATION_MESSAGE);
+        douNum1 = Double.parseDouble(strNum1);
+        douNum2 = Double.parseDouble(strNum2);
+
+        JOptionPane.showMessageDialog(null, "Sum of them is: "+(douNum1+douNum2), "Sum", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Difference of them is: " + (douNum1-douNum2), "Difference", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Product of them is: " + (douNum1*douNum2), "Product", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Quotient of them is: " + (douNum1/douNum2), "Quotient", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
